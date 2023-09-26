bb2c5ec474afd30bdf19e0e909fe37c965754ed1
postfm <postfm@yandex.ru>
2023-09-26 17:09:48 +0300

    Добавил импорт GenerateCommand

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,5 +1,10 @@
-import { CLIApplication, GenerateCommand } from './cli/index.js';
-import { HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+import { CLIApplication } from './cli/index.js';
+import {
+  HelpCommand,
+  ImportCommand,
+  VersionCommand,
+  GenerateCommand,
+} from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
