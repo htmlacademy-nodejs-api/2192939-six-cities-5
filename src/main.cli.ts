a7b130c0ff2429027e486e6bc31c84878fbf6650
postfm <postfm@yandex.ru>
2023-10-02 14:34:43 +0300

    Исправит ошибки динамического экспорта

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,11 +1,5 @@
 import { fileURLToPath } from 'node:url';
-import { CLIApplication } from './cli/index.js';
-import {
-  // HelpCommand,
-  ImportCommand,
-  VersionCommand,
-  GenerateCommand,
-} from './cli/index.js';
+import { CLIApplication, Command } from './cli/index.js';
 import { dirname, resolve } from 'node:path';
 import { readdirSync } from 'node:fs';
 
@@ -14,39 +8,29 @@ const regularExpression = new RegExp(/.command/);
 
 /**Точка входа приложения */
 async function bootstrap() {
-  /**Создает экземпляр класса  CLIApplication*/
-  const cliApplication = new CLIApplication();
-
+  /**Считываем все команды из директории ./commands */
   const filename = fileURLToPath(import.meta.url);
   const filepath = dirname(filename);
   const commandPath = resolve(filepath, COMMAND_DIR);
-  console.log(commandPath);
 
   const commandFileNames = readdirSync(commandPath)
     .filter((commandFileName) => regularExpression.test(commandFileName))
     .map((commandFileName) => commandFileName.replace('ts', 'js'));
+
+  const commands = [];
   for (let i = 0; i < commandFileNames.length; i++) {
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax
-    const commandName = await import(COMMAND_DIR + commandFileNames[i]);
-    console.log(commandName);
+    const commandName: Array<{ new (...args: unknown[]): Command }> =
+      await import(COMMAND_DIR + commandFileNames[i]);
 
     const [command] = Object.values(commandName);
-    console.log(command);
+    commands.push(new command());
   }
 
-  const path = `./cli/commands/help.command.js`;
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const help = await import(path);
-  const [command] = Object.values(help);
+  /**Создает экземпляр класса  CLIApplication*/
+  const cliApplication = new CLIApplication();
 
   /**Регистрирует команды */
-  cliApplication.registerCommands([
-    // new HelpCommand(),
-    new command(),
-    new VersionCommand(),
-    new ImportCommand(),
-    new GenerateCommand(),
-  ]);
+  cliApplication.registerCommands(commands);
 
   /**Запускает processCommand и передает в него все данные, переданные вызываемому скрипту */
   cliApplication.processCommand(process.argv);
