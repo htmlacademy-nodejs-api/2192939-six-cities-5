28c07ed9284740c4ed3b11c838a4e3626528f290
postfm <postfm@yandex.ru>
2023-10-15 19:33:02 +0300

    Добавит методы регистрации и входа для пользователя

--- src/shared/modules/user/user.controller.ts
+++ src/shared/modules/user/user.controller.ts
@@ -1,13 +1,27 @@
+import { UserService } from './index.js';
 import { inject, injectable } from 'inversify';
-import { BaseController, HttpMethod } from '../../libs/rest/index.js';
+import {
+  BaseController,
+  HttpError,
+  HttpMethod,
+} from '../../libs/rest/index.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
-import { NextFunction, Response } from 'express';
+import { Response } from 'express';
 import { CreateUserRequest } from './index.js';
+import { Config, RestSchema } from '../../libs/config/index.js';
+import { StatusCodes } from 'http-status-codes';
+import { fillDTO } from '../../helpers/common.js';
+import { UserRdo } from './rdo/user.rdo.js';
+import { LoginUserTypeRequest } from './login-user-login.type.js';
 
 @injectable()
 export class UserController extends BaseController {
-  constructor(@inject(Component.Logger) protected readonly logger: Logger) {
+  constructor(
+    @inject(Component.Logger) protected readonly logger: Logger,
+    @inject(Component.UserService) private readonly userService: UserService,
+    @inject(Component.Config) private readonly configService: Config<RestSchema>
+  ) {
     super(logger);
     this.logger.info('Register routes for UserController...');
 
@@ -16,13 +30,52 @@ export class UserController extends BaseController {
       method: HttpMethod.Post,
       handler: this.create,
     });
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Post,
+      handler: this.login,
+    });
   }
 
   public async create(
-    _req: CreateUserRequest,
-    _res: Response,
-    _next: NextFunction
+    { body }: CreateUserRequest,
+    res: Response
   ): Promise<void> {
-    throw new Error('[UseController] Oops');
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» already exists`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(
+      body,
+      this.configService.get('SALT')
+    );
+    this.created(res, fillDTO(UserRdo, result));
+  }
+
+  public async login(
+    { body }: LoginUserTypeRequest,
+    _res: Response
+  ): Promise<void> {
+    const existsUser = await this.userService.findByEmail(body.email);
+
+    if (!existsUser) {
+      throw new HttpError(
+        StatusCodes.UNAUTHORIZED,
+        `User with email «${body.email}» not found`,
+        'UserController'
+      );
+    }
+
+    throw new HttpError(
+      StatusCodes.NOT_IMPLEMENTED,
+      'Not implemented',
+      'UserController'
+    );
   }
 }
